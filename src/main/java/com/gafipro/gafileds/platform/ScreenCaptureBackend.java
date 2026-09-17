c42f8e992bef7df016b2884b8714ff5438ae374d
GafiPro <gabrielriosguedes@gmail.com>
2026-09-17 18:57:36 +0100

    Add render-thread fullscreen capture API

--- src/main/java/com/gafipro/gafileds/platform/ScreenCaptureBackend.java
+++ src/main/java/com/gafipro/gafileds/platform/ScreenCaptureBackend.java
@@ -2,5 +2,21 @@ package com.gafipro.gafileds.platform;
 
 import java.awt.Dimension;
 import java.awt.image.BufferedImage;
+import java.util.function.Consumer;
 
-public interface ScreenCaptureBackend extends AutoCloseable { BufferedImage capture() throws Exception; Dimension outputSize(); @Override void close(); }
+public interface ScreenCaptureBackend extends AutoCloseable {
+    BufferedImage capture() throws Exception;
+    Dimension outputSize();
+
+    /**
+     * Requests a fullscreen frame capture from the render thread.
+     * Implementations may invoke the consumer synchronously or asynchronously,
+     * but must invoke it with the newly captured frame when the request succeeds.
+     */
+    default boolean captureFullscreen(Consumer<BufferedImage> consumer) throws Exception {
+        return false;
+    }
+
+    @Override
+    void close();
+}
